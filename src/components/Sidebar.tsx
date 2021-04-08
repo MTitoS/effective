24e9bc6c7a3fc14c96ccf492348fcbdf19a3551c
Matheus Tito <68472398+MTitoS@users.noreply.github.com>
2021-04-08 17:22:42 -0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,11 +9,6 @@ export function Sidebar({ toggleTheme }: Props) {
   return (
     <div className={styles.container}>
       <img src="/sidebarLogo.svg" alt="Sidebar logo"/>
-      <div className={styles.icons}>
-        <button><img src="/icons/homeicon.png" alt="Home icon" /></button>
-        <br />
-        <button><img src="/icons/leaderb-icon.png" alt="Leaderboard icon" /></button>
-      </div>
       <div className={styles.switcher}>
         <ThemeSwitcher handleToggleTheme={toggleTheme}/>
       </div>
