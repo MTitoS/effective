12bac1b28a96993b6799e1ed2937ffb9847e18e3
Matheus Tito <mthsilva0@gmail.com>
2021-11-24 14:55:24 -0300

    Profile changed

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,16 +3,22 @@ import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
 
-    const {level} = useContext(ChallengesContext);
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://picsum.photos/200"
+        alt="Your profile picture"
+      />
+      <div>
+        <input id="nickname-input" type="text" maxLength="20" />
 
-    return (
-        <div className={styles.profileContainer}>
-            <img src="https://github.com/MTitoS.png" alt="Matheus Tito Foto" />
-            <div>
-                <strong>Matheus Tito</strong>
-                <p><img src="icons/level.svg" alt="Icone Level UP" /> Nível {level} </p>
-            </div>
-        </div>
-    )
+        <p>
+          <img src="icons/level.svg" alt="Icone Level UP" />{" "}
+          Nível {level}{" "}
+        </p>
+      </div>
+    </div>
+  );
 }
